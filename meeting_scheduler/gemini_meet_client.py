e293adcc6233fe82be7968e62de1edb26d098eaa
Mainak-learner <sarkar.mainak101@gmail.com>
2025-10-25 14:46:03 -0500

    fix meeting scheduler

--- meeting_scheduler/gemini_meet_client.py
+++ meeting_scheduler/gemini_meet_client.py
@@ -36,7 +36,6 @@ SAFETY_NONE = {
     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
     HarmCategory.HARM_CATEGORY_HATE_SPEECH:       HarmBlockThreshold.BLOCK_NONE,
     HarmCategory.HARM_CATEGORY_HARASSMENT:        HarmBlockThreshold.BLOCK_NONE,
-    HarmCategory.HARM_CATEGORY_SEXUAL_CONTENT:    HarmBlockThreshold.BLOCK_NONE,
 }
 
 # ---------- Time helpers ----------
